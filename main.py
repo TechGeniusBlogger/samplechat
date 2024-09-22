37fc7ba64d1ac50543f2d96ace7c54e7794bef5f
TechGeniusBlogger <89389727+TechGeniusBlogger@users.noreply.github.com>
2024-09-22 12:10:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import openai
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
-# Set up Azure credentials
-credential = DefaultAzureCredential()
-key_vault_name = "chattechgeniusblog "
-key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
-secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
+# # Set up Azure credentials
+# credential = DefaultAzureCredential()
+# key_vault_name = "chattechgeniusblog "
+# key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
+# secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
 
-# Retrieve OpenAI API key from Azure Key Vault
-openai_api_key = secret_client.get_secret("OpenAI-API-Key").value
+# # Retrieve OpenAI API key from Azure Key Vault
+# openai_api_key = secret_client.get_secret("OpenAI-API-Key").value
 
 # Set OpenAI API key
-openai.api_key = openai_api_key
+openai.api_key = f0b9cd46e2624e5aaa2cc0726c48bc3a
 
 # Function to generate a response from OpenAI
 def generate_response(prompt):
